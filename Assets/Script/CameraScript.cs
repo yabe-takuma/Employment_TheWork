a038e0d69a97847591a8dc726e022d11fffe8ff5
yabe-takuma <115063061+yabe-takuma@users.noreply.github.com>
2024-07-25 15:14:36 +0900

    就職作品を作っているところ

--- Assets/Script/CameraScript.cs
+++ Assets/Script/CameraScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    private GameObject player;  //プレイヤー情報格納用
+    private Vector3 offset;  //相対距離取得用
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        //unitychanの情報を取得
+        this.player = GameObject.Find("unitychan");
+
+        //MainCamera(自分自身)とplayerとの相対距離を求める
+        offset = transform.position - player.transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //新しいトランスフォームの値を代入する
+        transform.position = player.transform.position + offset;
+
+        //プレイヤーの向きと同じようにカメラの向きを変更する
+        transform.rotation = player.transform.rotation;
     }
 }
